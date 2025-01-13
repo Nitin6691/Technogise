cf8a94de624038b547c6355502a80cf4d4094cb6
nitinkumar <nitin6691@gmail.com>
2025-01-13 21:07:57 +0530

    Moved map for column character to column index mapping into ChessBoard class

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,16 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChessBoard {
     private static ChessBoard chessBoard;
 
     String[][] board = new String[8][8];
 
+    private final Map<String, Integer> map= new HashMap<>();
+
+
     private ChessBoard(){
         for (int i=0; i<8; i++) {
             for (int j = 0; j < 8; j++) {
                 board[i][j] = (Character.toString('A' + j))
-                        + Integer.toString(8 - i);
+                        + (8 - i);
             }
         }
-
+        map.put("A", 0);
+        map.put("B", 1);
+        map.put("C", 2);
+        map.put("D", 3);
+        map.put("E", 4);
+        map.put("F", 5);
+        map.put("G", 6);
+        map.put("H", 7);
     }
 
     public static ChessBoard getInstance() {
@@ -24,5 +37,7 @@ public class ChessBoard {
         return board[row][col];
     }
 
-    //Move map from peice class to here
+    public int getColumn(String pos){
+        return map.get(pos);
+    }
 }
--- src/Pawn.java
+++ src/Pawn.java
@@ -8,19 +8,9 @@ public class Pawn implements ChessPiece{
     public String getSteps(ChessBoard board, String position) {
         String result = "";
 
-        Map<String, Integer> map= new HashMap<>();
-        map.put("A", 0);
-        map.put("B", 1);
-        map.put("C", 2);
-        map.put("D", 3);
-        map.put("E", 4);
-        map.put("F", 5);
-        map.put("G", 6);
-        map.put("H", 7);
-
 
         String pos = String.valueOf(position.charAt(0));
-        int col = map.get(pos);
+        int col = board.getColumn(pos);
         int row = 8 - Integer.parseInt(String.valueOf(position.charAt(1)));
 
         if(row-1 >= 0)
